02996233ee26dc572497e1a927e1fa65c430a20c
cjlaborde <cjlaborde@gmail.com>
2018-12-14 22:13:56 -0400

    commit3

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -2,10 +2,10 @@ import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import DotEnv from 'dotenv'
 
-DotEnv.config({ path: '.env.test'  })
 
 Enzyme.configure({
     adapter: new Adapter()
 })
 
+DotEnv.config({ path: '.env.test'  })
 // https://jest-bot.github.io/jest/docs/configuration.html#setupfiles-arrayww
